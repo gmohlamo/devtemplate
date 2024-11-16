113c2f949dc07b88375d5c3122aaa4b32cd7e09c
Gladwin Tshepo Mohlamonyane <g.mohlamonyane@gmail.com>
2024-11-16 21:38:24 +0200

    installing python for the user directly

--- install.sh
+++ install.sh
@@ -11,11 +11,30 @@ mv jdk-22.0.2 ~/jdk
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash && source ~/.nvm/nvm.sh && nvm install --lts
+# build and install python
+cd /tmp/
+wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tgz
+tar xzf Python-3.13.0.tgz
+cd Python-3.13.0
+
+sudo ./configure --prefix=/opt/python/3.13.0/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi
+sudo make -j "$(nproc)"
+sudo make altinstall
+sudo rm /tmp/Python-3.13.0.tgz
+# install pip
+python3 -m ensurepip --upgrade
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+python3 -m pip install --user --upgrade pynvim
+# this should give us the python provider
+
+
 
 # install NvChad
 nvim --headless "+Lazy! update" +qa && nvim --headless +MasonInstallAll +qa
 # Setup JDK
 echo 'export PATH="$PATH:$HOME/jdk/bin"' >> ~/.bashrc
+echo "export JAVA_HOME=$HOME/.local/openjdk" >> ~/.bashrc
 echo 'alias vim=nvim' >> ~/.bashrc
 
 # Start listener
